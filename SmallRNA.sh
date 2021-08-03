d711135aa947992ab83971019b35a405f25037c4
XuanZhang <xzhangxmu@gmail.com>
2021-08-03 13:27:16 +1000

    Update SmallRNA.sh

--- SmallRNA.sh
+++ SmallRNA.sh
@@ -1,15 +1,50 @@
+#!/bin/bash
+
+#output:
+#change_list
+#correct_read.fastq
+
+H=5;
+T=8;
+O=correct_read.fasta;
+
+while getopts f:o:h:t: op
+do 
+    case $op in
+        f)
+            echo "Input file name is: $OPTARG"
+            F=$OPTARG;;
+        o)
+            echo "Output file name is: $OPTARG"
+            O=$OPTARG;;
+        h)
+            echo "Threshod value is: $OPTARG"
+            H=$OPTARG;;
+        t)
+            echo "The number of threads is: $OPTARG"
+            T=$OPTARG;;
+        \?)
+            echo "Usage: args [-h][-t][-f][-o] "
+            echo "-f means Input file name "
+            echo "-o means Output file name "
+            echo "-h means Threshod value"
+            echo "-t means the number of threads(Default:8)"
+            exit 1;;
+    esac
+done
+
+awk '{if(NR%2!=0)ORS=" ";else ORS="\n"}1' ${F} > ./rawread.txt
+
+echo "--------------start error correction ";
+
+./smallRNA_propor -k ${H}  -t ${T} -f ./rawread.txt -o ./${O}
 
 
-	awk '{if(NR%2!=0)ORS=" ";else ORS="\n"}1' /home/xuanzhan/Data/smallRNA/simu/m_d${j}/simumD${j}.fa > /home/xuanzhan/Data/smallRNA/simu/m_d${j}/rawread_D${j}.txt
+if [ "${O}" != "correct_read.fasta" ]
+then 
+	cp correct_read.fastq ${O};
+	rm correct_read.fasta
+fi
 
-	echo "--------------start error correction ";
-
-	./smallRNA_propor -k 10  -t 20 -f /home/xuanzhan/Data/smallRNA/simu/m_d${j}/rawread_D${j}.txt
-
-	mv correct_read.fastq /home/xuanzhan/Data/smallRNA/simu/m_d${j}/
-	mv changed_list.txt /home/xuanzhan/Data/smallRNA/simu/m_d${j}/
-
-	echo "--------------start ${j} evaluation ";
-	./evaluation -e /home/xuanzhan/Data/smallRNA/simu/m_d${j}/truth_D${j}.txt  -c /home/xuanzhan/Data/smallRNA/simu/m_d${j}/changed_list.txt
-
-
+# echo "--------------start  evaluation ";
+# ./evaluation -e /home/xuanzhan/Data/smallRNA/simu/m_d${j}/truth_D${j}.txt  -c /home/xuanzhan/Data/smallRNA/simu/m_d${j}/changed_list.txt
